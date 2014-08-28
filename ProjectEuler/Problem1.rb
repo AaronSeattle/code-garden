d4e68172ac4b90dabb1e4d6d7749ad9ae1cd84e9
Aaron Brooks <abrooks.uber@gmail.com>
2014-08-28 00:14:58 -0700

    minor cleanup of Problems 1 and 3

--- ProjectEuler/Problem1.rb
+++ ProjectEuler/Problem1.rb
@@ -11,5 +11,3 @@ end
 
 puts
 puts 'Sum: ' + sum.to_s
-
-  
--- ProjectEuler/Problem3.rb
+++ ProjectEuler/Problem3.rb
@@ -1,17 +1,6 @@
 puts 'Project Euler Problem 3: What is the largest prime factor of the number 600851475143 ?'
 
-#num = 30
-#num = 213 # 71 x 3
-#num = 2059 # 29 x 71
-#num = 13195 # 3 x 5 x 29
-#num = 80301 # 71 x 29 x 13 x 3
-#num = 401505 # 71 x 29 x 13 x 5 x 3
-#num = 1365117 # 71 x 29 x 17 x 13 x 3
-#num = 25937223 # 71 x 29 x 19 x 17 x 13 x 3
-#num = 181560561 # 71 x 29 x 19 x 17 x 13 x 7 x 3
-
 num = 600851475143
-#num = 775147 # sqrt rounded up of number from problem
 
 def largest_prime_factor(n)
 	largest_factor = 1
@@ -39,7 +28,7 @@ start = Time.now
 
 answer = largest_prime_factor(num)
 
-puts "Largest prime factor of #{num} is #{answer}."
-
 finish = Time.now
 puts "___ #{finish - start} seconds: Calculation completed "
+
+puts "Largest prime factor of #{num} is #{answer}."
